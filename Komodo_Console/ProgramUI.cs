7430aa128c5e13acf362e6bd6c9039afafde05e3
Jonathan Dikteruk <jonathan.dikteruk@gmail.com>
2020-12-03 23:20:04 -0500

    Initial Commit

--- Komodo_Console/ProgramUI.cs
+++ Komodo_Console/ProgramUI.cs
@@ -9,8 +9,11 @@ namespace Komodo_Console
 {
     class ProgramUI
     {
+        private DeveloperRepo _devRepo = new DeveloperRepo();
+        private DevTeamRepo _devTeamRepo = new DevTeamRepo();
         public void Run()
         {
+            SeedDeveloper();
             Menu();
         }
 
@@ -64,6 +67,7 @@ namespace Komodo_Console
                         break;
                     default:
                         Console.WriteLine("Please enter a valid option.");
+                        break;
                 }
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
@@ -73,43 +77,101 @@ namespace Komodo_Console
 
         private void CreateDeveloper()
         {
+            Console.Clear();
+
             Developer developer = new Developer();
+
+            // Name
+            Console.WriteLine("Enter Developer Name");
+            developer.Name = Console.ReadLine();
+
+            // ID
+            Console.WriteLine("Enter Developer ID");
+            string idAsString = Console.ReadLine();
+            developer.ID = int.Parse(idAsString);
+
+            // accessPluralsight
+            Console.WriteLine("Enter Developer Pluralsight Access Granted(Y/N)");
+            string accessGranted = Console.ReadLine().ToUpper();
+            
+            if(accessGranted == "Y")
+            {
+                developer.AccessPluralsight = true;
+            }
+            else
+            {
+                developer.AccessPluralsight = false;
+            }
+
+            _devRepo.AddDeveloper(developer);
         }
 
         private void ViewDevelopers()
         {
+            Console.Clear();
 
+            List<DeveloperRepo> developerList = _devRepo.GetDeveloperList();
+
+            foreach (Developer developer in developerList)
+            {
+                Console.WriteLine($"Name: {developer.Name}\n" +
+                    $"Developer ID: {developer.ID}\n" +
+                    $"Has access to Pluralsight: {developer.AccessPluralsight}")
+            }
         }
         
         private void UpdateDeveloper()
         {
-
+            Console.Clear();
         }
         
         private void DeleteDeveloper()
         {
-
+            Console.Clear();
         }
 
         private void CreateDevTeam()
         {
-
+            Console.Clear();
+            /*
+             * TeamName
+             * TeamID
+             * TeamMember
+             */*
         }
             
         private void ViewDevTeam()
         {
-
+            Console.Clear();
         }
 
         private void UpdateDevTeam()
         {
-
+            Console.Clear();
         }
             
         private void DeleteDevTeam()
         {
-
+            Console.Clear();
         }
         
+
+        private void SeedDeveloper()
+        {
+            Developer jon = new Developer("Jonathan Dikteruk", 1000001, false);
+            Developer thor = new Developer("Thor Dikteruk", 1000002, false);
+            Developer optimus = new Developer("Optimus Prime", 1000003, true);
+            Developer iron = new Developer("Iron Hide", 1000004, true);
+            Developer megs = new Developer("Mega Tron", 1000005, true);
+            Developer star = new Developer("Star Scream", 1000006, true);
+
+            _devRepo.AddDeveloper(jon);
+            _devRepo.AddDeveloper(thor);
+            _devRepo.AddDeveloper(optimus);
+            _devRepo.AddDeveloper(iron);
+            _devRepo.AddDeveloper(megs);
+            _devRepo.AddDeveloper(star);
+
+        }
     }
 }
